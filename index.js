f44f49803d063468c4666829d5fc9cac13819e4b
axetroy <troy450409405@gmail.com>
2018-01-11 16:52:02 +0800

    refactor: make sure defer func run right

--- index.js
+++ index.js
@@ -1,47 +1,60 @@
 const isFunction = require('lodash.isfunction');
 
-function defer(func) {
-  return function() {
+function isPromiseLike(obj) {
+  return obj && obj.then && typeof isFunction(obj.then);
+}
+
+/**
+ * run a queue task until it end
+ * @param queue
+ * @param ctx
+ * @param isAsync
+ * @returns {Promise.<T>}
+ */
+function runQueue(queue, ctx, isAsync) {
+  let cb = queue.pop();
+  if (isFunction(cb)) {
+    const r = cb(ctx);
+    if (isPromiseLike(r)) {
+      return r
+        .then(() => runQueue(queue, ctx, isAsync))
+        .catch(() => runQueue(queue, ctx, isAsync));
+    } else {
+      return runQueue(queue, ctx, isAsync);
+    }
+  } else {
+    return isAsync ? Promise.resolve(ctx) : ctx;
+  }
+}
+
+/**
+ * generate a defer function
+ * @param func
+ * @returns {defer}
+ */
+function godefer(func) {
+  return function defer() {
     const task = [];
 
-    // run function and get result
     const ret = func.apply(
       this,
       [].slice.call(arguments).concat([task.push.bind(task)])
     );
 
-    let cb;
-
-    if (ret && ret.then && typeof isFunction(ret.then)) {
+    if (isPromiseLike(ret)) {
       return ret
         .then(res => {
-          try {
-            while ((cb = task.pop())) {
-              isFunction(cb) && cb(res);
-            }
-          } catch (err) {
-            return Promise.reject(err);
-          }
-          return Promise.resolve(res);
+          return runQueue(task, res, true);
         })
         .catch(res => {
-          try {
-            while ((cb = task.pop())) {
-              isFunction(cb) && cb(res);
-            }
-          } catch (err) {
-            return Promise.reject(err);
-          }
-          return Promise.reject(res);
+          return runQueue(task, res, true).then(err => Promise.reject(err));
         });
     } else {
-      while ((cb = task.pop())) {
-        isFunction(cb) && cb(ret);
-      }
-      return ret;
+      return runQueue(task, ret, false);
     }
   };
 }
 
-module.exports = defer;
-module.exports.default = defer;
+module.exports = godefer;
+module.exports.godefer = godefer;
+module.exports.default = godefer;
